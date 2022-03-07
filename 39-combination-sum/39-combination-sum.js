1dff2b2e6c35dda7da1d0ca6f3786ea80371b840
kong <70005144+kongyb@users.noreply.github.com>
2022-03-07 14:15:45 +0900

    Time: 124 ms (47.37%), Space: 49.2 MB (13.21%) - LeetHub

--- 39-combination-sum/39-combination-sum.js
+++ 39-combination-sum/39-combination-sum.js
@@ -3,30 +3,54 @@
  * @param {number} target
  * @return {number[][]}
  */
-function uniqueArr(arr){
-    let result=[];
-    for(const subArr of arr){
-        let str=subArr.sort().join(' ');
-        if(!result.includes(str)){
-            result.push(str);
-        }
+// function uniqueArr(arr){
+//     let result=[];
+//     for(const subArr of arr){
+//         let str=subArr.sort().join(' ');
+//         if(!result.includes(str)){
+//             result.push(str);
+//         }
+//     }
+//     return result.map(el=>el.split(' '));
+// }
+
+// var combinationSum = function(candidates, target) {
+//     let sumArr=new Array(target+1)
+//     sumArr[0]=[[]];
+//     for(let i=1;i<=target;i++){
+//         sumArr[i]=[];
+//         for(const num of candidates){
+//             if(i-num<0){
+//                 continue;
+//             }
+//             else{
+//                 sumArr[i]=sumArr[i].concat(sumArr[i-num].map(el=>[...el,num]));
+//             }
+//         }
+//     }
+//     return uniqueArr(sumArr[target]);
+// };
+
+function findArrSum(candidate,target,prev,result){
+    if(target===0){
+        result.push(prev)
+        return
     }
-    return result.map(el=>el.split(' '));
+    if(candidate.length===0){
+        return;
+    }
+    let num=candidate.shift()
+    while(target>=0){
+        findArrSum(candidate,target,[...prev],result)
+        target-=num
+        prev.push(num)
+    }
+    candidate.unshift(num)
+    return
 }
 
 var combinationSum = function(candidates, target) {
-    let sumArr=new Array(target+1)
-    sumArr[0]=[[]];
-    for(let i=1;i<=target;i++){
-        sumArr[i]=[];
-        for(const num of candidates){
-            if(i-num<0){
-                continue;
-            }
-            else{
-                sumArr[i]=sumArr[i].concat(sumArr[i-num].map(el=>[...el,num]));
-            }
-        }
-    }
-    return uniqueArr(sumArr[target]);
-};
+    let result=[];
+    findArrSum(candidates,target,[],result)
+    return result
+}
