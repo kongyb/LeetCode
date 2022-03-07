f9ee31f8eb9cc334b0f993f942f84323c8aa0f4b
kong <70005144+kongyb@users.noreply.github.com>
2022-03-07 13:50:01 +0900

    Time: 76 ms (97.01%), Space: 46.1 MB (45.92%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.js
+++ 47-permutations-ii/47-permutations-ii.js
@@ -2,27 +2,50 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-function permutation(arr){
-    if(arr.length===1){
-        return [[arr[0]]]
+// function permutation(arr){
+//     if(arr.length===1){
+//         return [[arr[0]]]
+//     }
+//     let result=[]
+//     for(let i=0;i<arr.length;i++){
+//         let temp=permutation(arr.filter((el,index)=>{return index!==i}))
+//         result=result.concat(temp.map(el=>[arr[i],...el]))
+//     }
+//     return result
+// }
+
+// var permuteUnique = function(nums) {
+//     let uniqueSet=new Set()
+//     let totalPermutation=permutation(nums)
+//     totalPermutation.forEach((arr)=>{
+//         uniqueSet.add(arr.join(' '))
+//     })
+//     let result=Array.from(uniqueSet)
+//     return result.map(el=>{
+//         let temp=el.split(' ')
+//         return temp.map(num=>Number(num))
+//     })
+// };
+function permutation(nums,isChecked,prev,result){
+    if(prev.length===nums.length){
+        result.push(prev)
     }
-    let result=[]
-    for(let i=0;i<arr.length;i++){
-        let temp=permutation(arr.filter((el,index)=>{return index!==i}))
-        result=result.concat(temp.map(el=>[arr[i],...el]))
+    for(let i=0;i<nums.length;i++){
+        if(!isChecked[i]){
+            isChecked[i]=true
+            permutation(nums,isChecked,[...prev,nums[i]],result)
+            isChecked[i]=false
+            while(nums[i]===nums[i+1]){
+                i++
+            }
+        }
     }
-    return result
 }
 
 var permuteUnique = function(nums) {
-    let uniqueSet=new Set()
-    let totalPermutation=permutation(nums)
-    totalPermutation.forEach((arr)=>{
-        uniqueSet.add(arr.join(' '))
-    })
-    let result=Array.from(uniqueSet)
-    return result.map(el=>{
-        let temp=el.split(' ')
-        return temp.map(num=>Number(num))
-    })
+    let result=[];
+    nums.sort((a,b)=>a-b)
+    let isChecked=new Array(nums.length).fill(false);
+    permutation(nums,isChecked,[],result)
+    return result
 };
