cae2fc21514dee83df22d45356a18c39ba6d2ae3
kong <70005144+kongyb@users.noreply.github.com>
2022-03-09 18:19:17 +0900

    Time: 79 ms (55.68%), Space: 42.4 MB (16.70%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.js
+++ 413-arithmetic-slices/413-arithmetic-slices.js
@@ -6,17 +6,23 @@ var numberOfArithmeticSlices = function(nums) {
     if(nums.length<3){
         return 0
     }
-    let result=0
-    for(let left=0;left<nums.length-2;left++){
-        let right=left+2
-        if(nums[left+1]-nums[left]===nums[right]-nums[left+1]){
-            result+=right-left-1
-            while(nums[right+1]-nums[right]===nums[left+1]-nums[left]){
-                right++;
-                result+=right-left-1
+    let l=0,r=2,cnt=0
+    while(r<nums.length){
+        if(nums[l+1]-nums[l]===nums[r]-nums[r-1]){
+            while(nums[l+1]-nums[l]===nums[r+1]-nums[r]){
+                r++
             }
-            left=right-1
+            while(l<=r-2){
+                cnt+=r-l-1
+                l++
+            }
+            l=r
+            r=l+2
+        }
+        else{
+            l++
+            r++
         }
     }
-    return result
+    return cnt
 };
