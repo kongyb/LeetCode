8d8c609f0415c75a23add633c3993ea7a41b036f
kong <70005144+kongyb@users.noreply.github.com>
2022-03-14 18:47:38 +0900

    Time: 129 ms (63.28%), Space: 43.7 MB (66.41%) - LeetHub

--- 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.js
+++ 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.js
@@ -3,35 +3,27 @@
  * @return {number}
  */
 var findNumberOfLIS = function(nums) {
-    let cntArr=new Array(nums.length).fill(0)
     let lenArr=new Array(nums.length).fill(0)
-    let len=1
-    cntArr[0]=1,lenArr[0]=1;
-    for(let i=1;i<nums.length;i++){
-        //num보다 작은 원소들의 길이중 제일 긴 길이+1
-        // 제일 긴 길이의 갯수들의 합
-        let num=nums[i]
-        let maxLen=lenArr[0]
-        for(let j=0;j<i;j++){
-            if(nums[j]<num&&lenArr[j]+1>maxLen){
-                maxLen=lenArr[j]+1
-            }
-        }
-        lenArr[i]=maxLen
-        let sum=0
-        for(let j=0;j<i;j++){
-            if(lenArr[j]===lenArr[i]-1&&nums[j]<nums[i]){
-                sum+=cntArr[j]
-            }
-        }
-        cntArr[i]=sum===0?1:sum
-        if(maxLen>len){len=maxLen}
-    }
-    let result=0
+    let cntArr=new Array(nums.length).fill(0)
     for(let i=0;i<nums.length;i++){
-        if(lenArr[i]===len){
-            result+=cntArr[i]
+        lenArr[i]=1
+        cntArr[i]=1
+        for(let j=0;j<i;j++){
+            if(nums[i]>nums[j]){
+                if(lenArr[i]===lenArr[j]+1){
+                    cntArr[i]+=cntArr[j]
+                }
+                else if(lenArr[i]<lenArr[j]+1){
+                    lenArr[i]=lenArr[j]+1
+                    cntArr[i]=cntArr[j]
+                }
+            }
         }
+    }
+    let maxLen=Math.max(...lenArr)
+    let result=0
+    for(let i=0;i<cntArr.length;i++){
+        if(lenArr[i]===maxLen){result+=cntArr[i]}
     }
     return result
 };
