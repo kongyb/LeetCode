59fa793c1c81d3c0b404e933cdd0a49aa9def606
kong <70005144+kongyb@users.noreply.github.com>
2022-03-23 16:43:44 +0900

    Time: 398 ms (65.85%), Space: 56.7 MB (85.71%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.js
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.js
@@ -5,12 +5,9 @@
 var minPairSum = function(nums) {
     nums.sort((a,b)=>a-b)
     let max=0
-    // while(nums.length>0){
-    //     let sum=nums.shift()+nums.pop()
-    //     max=max>sum?max:sum
-    // }
     for(let i=0;i<nums.length/2;i++){
-        max=max>(nums[i]+nums[nums.length-1-i])?max:(nums[i]+nums[nums.length-1-i])
+        let sum=nums[i]+nums[nums.length-1-i]
+        max=Math.max(max,sum)
     }
     return max
 };
