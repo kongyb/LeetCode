b30b01b7a051a8cec77a3081dafb5f1a8673deeb
kong <70005144+kongyb@users.noreply.github.com>
2022-03-25 15:47:24 +0900

    Time: 94 ms (25.64%), Space: 41.9 MB (76.92%) - LeetHub

--- 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.js
+++ 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.js
@@ -3,16 +3,12 @@
  * @return {boolean}
  */
 var checkPowersOfThree = function(n) {
-    let arr=new Array(n+1).fill(0)
-    arr[1]=1
-    for(let i=1;i<=n/3;i++){
-        if(arr[i]===1){
-            arr[3*i]=1
-            arr[3*i+1]=1
+    while(n>0){
+        let rest=n%3
+        if(rest===2){
+            return false
         }
+        n=Math.floor(n/3)
     }
-    if(arr[n]===1){
-        return true
-    }
-    return false
+    return true
 };
